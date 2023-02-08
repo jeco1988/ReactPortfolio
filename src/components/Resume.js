c6951658864b89bd66b915703a7ff08afbec98ec
Jack <jackeconomos@outlook.com>
2023-02-08 14:57:54 +1030

    updated resume link

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -9,7 +9,7 @@ function Resume() {
 
         <a
           className="button is-primary"
-          href={process.env.PUBLIC_URL + "/public/img/jackeconomosresume.pdf"}
+          href={"https://github.com/jeco1988/reactportfolio/raw/main/public/img/jackeconomosresume.pdf"}
           target="_blank" rel="noreferrer"
         >
           <span className="icon">
